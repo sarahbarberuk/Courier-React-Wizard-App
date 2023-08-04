16c1e37263d7f5a9646d338cab7a5113211448f5
Sarah Barber <sarahbarberuk@gmail.com>
2023-08-04 15:49:41 +0100

    Fixed spell alignment issue, Removed duplicate spell image files

--- courier-email-client/src/Components/Spellbook.js
+++ courier-email-client/src/Components/Spellbook.js
@@ -2,11 +2,7 @@
 import React from "react";
 import Spell from "./Spell";
 import spellbookImage from "../spellbook.png";
-import spell1Image from "../spell1.png";
-import spell2Image from "../spell2.png";
-import spell3Image from "../spell3.png";
-import spell4Image from "../spell4.png";
-import spell5Image from "../spell5.png";
+import spellImage from "../spellImage.png";
 
 const Spellbook = () => {
   return (
@@ -18,21 +14,21 @@ const Spellbook = () => {
             spellName="Wizard Initiation"
             email="YOUR_EMAIL"
             templateId="YOUR_TEMPLATE_ID"
-            spellImage={spell1Image}
+            spellImage={spellImage}
             description="A warm magical gust of welcoming wind, introducing the newly initiated wizard to our mystical realm. A spell that sends arcane greetings to wizards who have just begun their journey."
           />
           <Spell
             spellName="Lost Spell"
             email="YOUR_EMAIL"
             templateId="YOUR_TEMPLATE_ID"
-            spellImage={spell2Image}
+            spellImage={spellImage}
             description="In the twisting labyrinth of magic, even the best wizards misplace their spells. A spell to restore forgotten secrets and recover access to your magical arsenal."
           />
           <Spell
             spellName="Spell Purchase"
             email="YOUR_EMAIL"
             templateId="YOUR_TEMPLATE_ID"
-            spellImage={spell3Image}
+            spellImage={spellImage}
             description="A binding pact of magical trade, confirming your acquisition of our potent enchantments. This spell whispers an arcane confirmation of your newly purchased powers."
           />
         </div>
@@ -41,14 +37,14 @@ const Spellbook = () => {
             spellName="Spell Delivery"
             email="YOUR_EMAIL"
             templateId="YOUR_TEMPLATE_ID"
-            spellImage={spell4Image}
+            spellImage={spellImage}
             description="Swift as an owl's flight, this spell ensures your newly acquired enchantments find their way to you. A spell of tracking and notifying the journey of your mystical purchases"
           />
           <Spell
             spellName="Potion Replenishment"
             email="YOUR_EMAIL"
             templateId="YOUR_TEMPLATE_ID"
-            spellImage={spell5Image}
+            spellImage={spellImage}
             description="Ensuring your potion stores never run dry, this recurring enchantment renews your reserves of magical brews. A spell that keeps your subscription potions flowing like a magical spring"
           />
         </div>
